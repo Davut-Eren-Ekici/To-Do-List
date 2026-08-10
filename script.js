56624048eac4c01464155e89cb73fca72f471d0b
Davut Eren Ekici <davuteren052@gmail.com>
2026-08-10 18:28:53 +0300

    Add task completion, persistence and deletion

--- script.js
+++ script.js
@@ -1,31 +1,88 @@
 const tasks = [];
 
-console.log(tasks);
+const savedTasks = localStorage.getItem("tasks");
+const parsedTasks = JSON.parse(savedTasks);
+
+
+
 
 const taskList = document.querySelector("#taskList");
 const AddBtn = document.querySelector("#AddBtn");
 const taskInput = document.querySelector("#taskInput");
+const completedList = document.querySelector("#completedList");
+
+tasks.push(...parsedTasks);
+renderTasks();
 
 AddBtn.addEventListener("click", function(){
+    if(taskInput.value.trim() == ""){
+        return;
+    }
+
     const task ={
         text: taskInput.value,
         completed: false
         
     };
-    renderTasks();
     tasks.push(task);
-    
-    const li = document.createElement("li");
-    li.textContent = task.text;
-    taskList.appendChild(li);
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+    renderTasks();
     taskInput.value = "";
+    
+    
 
 });
 
 function renderTasks(){
+    taskList.innerHTML = "";
+    completedList.innerHTML = "";
+
     for (let i=0;i<tasks.length;i++){
+
         const li =  document.createElement("li");
-        li.textContent = tasks[i].text;
-        taskList.appendChild(li);
+       
+
+        if (tasks[i].completed) {
+
+         li.classList.add("completed");
+        completedList.appendChild(li);
+
+        } else {
+
+         taskList.appendChild(li);
+
+        }
+
+        const completeBtn = document.createElement("button");
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent="sil";
+
+        if (tasks[i].completed) {
+            completeBtn.textContent = "✓";
+        } else {
+           completeBtn.textContent = "○";
+        }
+        
+        completeBtn.addEventListener("click", function(){
+            tasks[i].completed = !tasks[i].completed;
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            renderTasks();
+        });
+
+        deleteBtn.addEventListener("click",function(){
+            tasks.splice(i,1);
+            localStorage.setItem("tasks",JSON.stringify(tasks));
+            renderTasks();
+        });
+        
+        li.appendChild(completeBtn);
+        li.appendChild(deleteBtn);
+        
+
+        const taskText = document.createElement("span");
+        taskText.textContent = tasks[i].text;
+       li.appendChild(taskText);
+       
+
     }
 }
